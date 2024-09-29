d6d2b0cae92570f6973e611179c6284494853fe4
Somar Hassan <somarhassan322@gmail.com>
2024-09-29 15:11:59 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import Navbar from './Navbar';
+import Home from './Home';
+import { BrowserRouter as Router ,Route , Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import Create from './Create';
+import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
+
+function App() {
+//sfc
+// npm run start
+//npx json-server --watch data/db.json --port 8000
+
+  return (
+    <Router>
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <Switch>
+          <Route exact path="/">
+          <Home/>
+          </Route>
+          <Route path="/create">
+          <Create/>
+          </Route>
+          <Route path="/blogs/:id">
+          <BlogDetails/>
+          </Route>
+          <Route path="*">
+            <NotFound/>
+          </Route>
+        </Switch>
+
+      </div>
+    </div>
+    </Router>
+  );
+}
+
+export default App;
